5c6d4f08e791f32d3095f49b2dcc3b4602474d05
shmuel10 <44431762+shmuel10@users.noreply.github.com>
2022-03-06 01:04:16 +0200

    Update get_pkg_list.py

--- WindowsScanner/get_pkg_list.py
+++ WindowsScanner/get_pkg_list.py
@@ -12,24 +12,24 @@ def get_installed_packages_details():
                                          access=winreg.KEY_READ)
 
         index = 0
-            try:
-                while True:
-                    # Generates the full path in Registry of each installed pkg
-                    installed_pkg_path = PATH_TO_INSTALLED_PACKAGES + '\\' + winreg.EnumKey(packages_handle, index)
+        try:
+            while True:
+                # Generates the full path in Registry of each installed pkg
+                installed_pkg_path = PATH_TO_INSTALLED_PACKAGES + '\\' + winreg.EnumKey(packages_handle, index)
 
-                    # Retrieves the values of each installed pkg
-                    pkg_details = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, installed_pkg_path, reserved=0,
-                                                 access=winreg.KEY_READ)
-                    try:
-                        pkg_name = winreg.QueryValueEx(pkg_details, "DisplayName")[0]
-                        pkg_version = winreg.QueryValueEx(pkg_details, "DisplayVersion")[0]
-                        result.append((pkg_name, pkg_version))
-                    except OSError as e:
-                        pass
+                # Retrieves the values of each installed pkg
+                pkg_details = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, installed_pkg_path, reserved=0,
+                                             access=winreg.KEY_READ)
+                try:
+                    pkg_name = winreg.QueryValueEx(pkg_details, "DisplayName")[0]
+                    pkg_version = winreg.QueryValueEx(pkg_details, "DisplayVersion")[0]
+                    result.append((pkg_name, pkg_version))
+                except OSError as e:
+                    pass
 
-                    index += 1
-            except OSError as e:
-                return result
+                index += 1
+        except OSError as e:
+            return result
     except Exception as e:
         raise e
 
