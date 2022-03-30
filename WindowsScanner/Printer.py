2eabe1a930b1cc3b880bea7c7d78377305bdd913
yosefN <yosef.naftali@checkmarx.com>
2022-03-31 01:25:08 +0300

    Added support in deferent format (tuple)

--- WindowsScanner/Printer.py
+++ WindowsScanner/Printer.py
@@ -14,7 +14,22 @@ class Printer:
                 pass
         return result
 
+    def _tuple_to_list_converter(self, data_dict):
+        result = []
+        for item in data_dict:
+            try:
+                temp = ['  '.join(item[0]), '\n'.join(item[1])]
+                result.append(temp)
+            except Exception as e:
+                pass
+        return result
+
     def print(self, data, headers):
         data_list = self._dic_to_list_converter(data)
+        table = tabulate(data_list, headers=headers, tablefmt='grid')
+        print(table)
+
+    def print_tuple_to_chart(self, data, headers):
+        data_list = self._tuple_to_list_converter(data)
         table = tabulate(data_list, headers=headers, tablefmt='grid')
         print(table)
