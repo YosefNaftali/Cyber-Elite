dc3723cd2c5527fd96c54078b54444686b1bb7a0
yosefN <yosef.naftali@checkmarx.com>
2022-03-31 03:51:40 +0300

    Added configuration of RUN_EXPANDED_SCAN

--- WindowsScanner/Search.py
+++ WindowsScanner/Search.py
@@ -1,7 +1,7 @@
 from CpeTransformer import CpeTransformer
 from InstalledPkgMiner import InstalledPkgMiner
 from NvdClient import NvdClient
-
+RUN_EXPANDED_SCAN = 'True'
 miner = InstalledPkgMiner()
 transformer = CpeTransformer()
 nvd_client = NvdClient()
@@ -14,11 +14,14 @@ class Search:
     def search_machine(self):
         result = []
         installed_packages = miner.get_installed_packages_details()
-        installed_packages = [('7-zip', '3.13'), ('7-zip', '4.20'), ('excel', '*')]
+        installed_packages = [('7-zip (x64)', '3.13'), ('7-zip', '4.20'), ('excel', '*')]
         packages_cpe_schema = transformer.get_cpe_schema_for_packages(installed_packages)
         for cpe_schema in packages_cpe_schema:
             try:
-                cpes = nvd_client.get_cpes_from_nvd(cpe_schema[1])
+                if RUN_EXPANDED_SCAN == 'True':
+                    cpes = nvd_client.get_expended_cpes_from_nvd(cpe_schema[1])
+                else:
+                    cpes = nvd_client.get_cpes_from_nvd(cpe_schema[1])
                 cves = nvd_client.get_matches_cve_to_cpe_list(cpes)
                 result.append((cpe_schema[0], cves))
             except Exception as e:
