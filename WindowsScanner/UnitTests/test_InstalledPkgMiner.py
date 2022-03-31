efd5281681d1ef6da75b899e323577a22ae6f6fd
yosefN <yosef.naftali@checkmarx.com>
2022-03-31 11:14:49 +0300

    Unit Test/InstalledPkgMiner: print installed pkg

--- WindowsScanner/UnitTests/test_InstalledPkgMiner.py
+++ WindowsScanner/UnitTests/test_InstalledPkgMiner.py
@@ -9,3 +9,8 @@ class TestInstalledPkgMiner(TestCase):
     def test_get_installed_packages_details(self):
         installed_packages_details = installedPkgMiner.get_installed_packages_details()
         self.assertTrue(len(installed_packages_details) > 1)
+
+    def test_prnit_get_installed_packages_details(self):
+        installed_packages_details = installedPkgMiner.get_installed_packages_details()
+        print(installed_packages_details)
+        self.assertTrue(len(installed_packages_details) > 1)
